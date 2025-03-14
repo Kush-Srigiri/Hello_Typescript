dc5581a8c28522ff835d52cc547f2bb1f78dd16f
Kush <kushsrigiri@gmail.com>
2025-03-14 15:40:51 +0100

    [General]  | Files: package.json, src/rounding.test.ts, src/utils.test.ts, src/utils.ts

--- src/rounding.test.ts
+++ src/rounding.test.ts
@@ -4,4 +4,15 @@ test("check round to nickels", () => {
   expect(roundTo(10.0 / 3.0, 0.05)).toBe(3.35);
 });
 
+test("Rundung auf 0.01 (Cents)", () => {
+    expect(roundTo(1.234, 0.01)).toBe(1.23);
+    expect(roundTo(1.236, 0.01)).toBe(1.24);
+});
+
+test("Rundung auf 0.1 (Dimes)", () => {
+    expect(roundTo(1.24, 0.1)).toBe(1.2);
+    expect(roundTo(1.25, 0.1)).toBe(1.3);
+});
+
+
 
